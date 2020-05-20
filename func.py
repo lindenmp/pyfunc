ff1691b1eeb256645006efb9382fa1c785f33a1d
Linden Parkes <lindenparkes@gmail.com>
2020-05-20 17:32:17 -0400

    added ave_control_alt, which uses a different normalization strategy

--- func.py
+++ func.py
@@ -292,6 +292,41 @@ def ave_control(A, c = 1):
     return values
 
 
+def ave_control_alt(A, c = 0.99):
+    # FUNCTION:
+    #         Returns values of AVERAGE CONTROLLABILITY for each node in a
+    #         network, given the adjacency matrix for that network. Average
+    #         controllability measures the ease by which input at that node can
+    #         steer the system into many easily-reachable states.
+    #
+    # INPUT:
+    #         A is the structural (NOT FUNCTIONAL) network adjacency matrix, 
+    #         such that the simple linear model of dynamics outlined in the 
+    #         reference is an accurate estimate of brain state fluctuations. 
+    #         Assumes all values in the matrix are positive, and that the 
+    #         matrix is symmetric.
+    #
+    # OUTPUT:
+    #         Vector of average controllability values for each node
+    #
+    # Bassett Lab, University of Pennsylvania, 2016.
+    # Reference: Gu, Pasqualetti, Cieslak, Telesford, Yu, Kahn, Medaglia,
+    #            Vettel, Miller, Grafton & Bassett, Nature Communications
+    #            6:8414, 2015.
+
+    u, s, vt = svd(A) # singluar value decomposition
+    A = (A/s[0])*c # Matrix normalization
+    T, U = schur(A,'real') # Schur stability
+    midMat = np.multiply(U,U).transpose()
+    v = np.matrix(np.diag(T)).transpose()
+    N = A.shape[0]
+    P = np.diag(1 - np.matmul(v,v.transpose()))
+    P = repmat(P.reshape([N,1]), 1, N)
+    values = sum(np.divide(midMat,P))
+    
+    return values
+
+
 def modal_control(A, c = 1):
     # FUNCTION:
     #         Returns values of MODAL CONTROLLABILITY for each node in a
